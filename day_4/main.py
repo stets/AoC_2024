e00dd80ca0070db12d2c13893353c9d60f94373b
stets <stetsblake@gmail.com>
2024-12-19 22:35:01 -0500

    solve day 4 part 2

--- day_4/main.py
+++ day_4/main.py
@@ -1,9 +1,15 @@
+# AoC 2024 Day 4
+# todo: use logging or do something else so we
+# \can tune the outputs for exceptions
+# or avoid them altogether.
+
 class Parser():
     def __init__(self):
         with open('input.txt', 'r') as f:
             input = f.readlines()
         self.input = input
         self.sum = 0
+        self.xmas_crosses = 0
 
     def check_horizontal(self):
         for line in self.input:
@@ -25,14 +31,14 @@ class Parser():
             for x_idx, char in enumerate(line):
                 try:
                     candidate = (
-                                    self.input[y_idx][x_idx] +
+                                    char +
                                     self.input[y_idx+1][x_idx] +
                                     self.input[y_idx+2][x_idx] +
                                     self.input[y_idx+3][x_idx]
                     )
-                    print(candidate)
                 except Exception as e:
-                    print(f"exception {e}")
+                    # print(f"exception {e}")
+                    pass
                 if candidate == "XMAS":
                     self.sum += 1
                 elif candidate == "SAMX":
@@ -43,14 +49,14 @@ class Parser():
             for x_idx, char in enumerate(line):
                 try:
                     candidate = (
-                                    self.input[y_idx][x_idx] +
+                                    char +
                                     self.input[y_idx+1][x_idx+1] +
                                     self.input[y_idx+2][x_idx+2] +
                                     self.input[y_idx+3][x_idx+3]
                     )
-                    print(candidate)
                 except Exception as e:
-                    print(f"exception {e}")
+                    # print(f"exception {e}")
+                    pass
                 if candidate == "XMAS":
                     self.sum += 1
                 elif candidate == "SAMX":
@@ -61,18 +67,47 @@ class Parser():
             for x_idx, char in enumerate(line):
                 try:
                     candidate = (
-                                    self.input[y_idx][x_idx] +
+                                    char +
                                     self.input[y_idx+1][x_idx-1] +
                                     self.input[y_idx+2][x_idx-2] +
                                     self.input[y_idx+3][x_idx-3]
                     )
-                    print(candidate)
                 except Exception as e:
-                    print(f"exception {e}")
+                    # print(f"exception {e}")
+                    pass
                 if candidate == "XMAS":
                     self.sum += 1
                 elif candidate == "SAMX":
                     self.sum += 1
+
+    def find_cross_xmas(self):
+        # if we find an A...then check if there is a MAS
+        # check if the x-1, y-1 + self + x+1, y+1 == SAM or MAS and SAM or MAS
+        for y_idx, line in enumerate(self.input):
+            for x_idx, char in enumerate(line):
+                if x_idx >= 1 and x_idx <= 139 and y_idx >= 1 and y_idx <= 139:
+                    if char == "A":
+                        try:
+                            left_slant = (
+                                            self.input[y_idx-1][x_idx-1] +
+                                            char +
+                                            self.input[y_idx+1][x_idx+1]
+                            )
+                            right_slant = (
+                                            self.input[y_idx-1][x_idx+1] +
+                                            char +
+                                            self.input[y_idx+1][x_idx-1]
+                            )
+                        except Exception as e:
+                            # print(f"exception {e}")
+                            pass
+                        if (
+                            (left_slant == "MAS" or left_slant == "SAM") and
+                            (right_slant == "MAS" or right_slant == "SAM")
+                        ):
+                            self.xmas_crosses += 1
+
+        print(f"Day 4 Part 2 answer is {self.xmas_crosses}")
 
 
 if __name__ == '__main__':
@@ -82,3 +117,4 @@ if __name__ == '__main__':
     parser.check_diagonal_left_slant()
     parser.check_diagonal_right_slant()
     print(f"Day 4 Part 1 answer {parser.sum}")
+    parser.find_cross_xmas()
